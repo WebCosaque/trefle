4ac6073dd4595a3ba17565523a38ce3ac6c4bf40
WebCosaque <47447254+WebCosaque@users.noreply.github.com>
2019-02-09 15:38:10 +0100

    - Added the total size of the downloaded files - Added colors

--- trefle.py
+++ trefle.py
@@ -1,16 +1,22 @@
 # trefle 4chan files downloader.
 
 import requests
-import random
 import urllib.request
 import pyprind
-import time,datetime,os
+import time, datetime, os
 from hurry.filesize import size, si
 from bs4 import BeautifulSoup
-
 from pyfiglet import Figlet
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
 custom_fig = Figlet(font='ogre')
-print(custom_fig.renderText('trefle'))
+print(Back.GREEN + custom_fig.renderText('trefle'))
+
+print(Fore.WHITE + Back.GREEN + 'A tool to download all media files from a 4chan(nel).org thread.')
+print(Style.RESET_ALL)
+
 
 url = input('Thread URL: ')
 folder = input('Name of the folder (leave blank for an automated name) :')
@@ -36,4 +42,12 @@ for link in soup.findAll('a', {'class': 'fileThumb'}):
     path_name = str(folder) + str(full_name)
     urllib.request.urlretrieve(imageUrl, path_name)
 
-print('Done')
+total_size = 0
+for path, dirs, files in os.walk(folder):
+    for f in files:
+        fp = os.path.join(path, f)
+        total_size += os.path.getsize(fp)
+print(Back.CYAN + size(total_size, system=si), 'downloaded in', folder)
+print(Style.RESET_ALL)
+print(Fore.WHITE + Back.GREEN + 'Success !')
+print(Style.RESET_ALL)
